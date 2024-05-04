e747505e348ddbc6a30b6214d46c8cb8212a02dd
Som-Cillindr <danpapx@gmail.com>
2024-05-04 20:56:34 +1000

    папой

--- assets/js/room.js
+++ assets/js/room.js
@@ -1,35 +1,56 @@
+
 let room = document.querySelector(".room_main")
 
+import { professionMas } from "./Mas-js/prof.js";
+
+import { namesMas } from "./Mas-js/names.js";
+
+import { diseaseMas } from "./Mas-js/disease.js";
+
+import { hobbyMas } from "./Mas-js/hobby.js";
+import { fobosMas } from "./Mas-js/fobos.js";
+import { abilityMas } from "./Mas-js/ability.js";
+
+function random(min=0, max=199) {
+  return Math.floor(Math.random() * (max-min+1) + min)
+
+}
+
+// console.log(professionMas[random(0,  professionMas.length)]);
 
 class Player{
-  constructor(name, profession, workExp, disease, hobby, fobos, baggage, ability ){
-    this.name = name
+  constructor(nameArg, ageArg, professionArg, workExpArg, diseaseArg, hobbyArg, fobosArg, baggageArg, abilityArg ){
+    this.name = nameArg
+    this.age={
+      age: ageArg,
+      isVisible: false
+    }
     this.profession = {
-      profession: profession,
+      profession: professionArg,
       isVisible: false
     }
     this.workExp= {
-      workExp: workExp,
+      workExp: workExpArg,
       isVisible: false
     }
     this.disease= {
-      disease: disease,
+      disease: diseaseArg,
       isVisible: false
     }
     this.hobby= {
-      hobby: hobby,
+      hobby: hobbyArg,
       isVisible: false
     }
     this.fobos= {
-      fobos: fobos,
+      fobos: fobosArg,
       isVisible: false
     }
     this.baggage= {
-      baggage: baggage,
+      baggage: baggageArg,
       isVisible: false
     }
     this.ability= {
-      ability: ability,
+      ability: abilityArg,
       isVisible: false
     }
   }
@@ -42,45 +63,59 @@ class Player{
 }
 
 
-let player_1 = new Player(
-  "Папой", "Диктатор", 52, "немой", "гольф", "народ", "микрофон", "красивый"
-)
-let player_2 = new Player(
-  "Папой1", "Диктатор1", 521, "немой1", "гольф1", "народ1", "микрофон1", "красивый1"
-)
-let player_3 = new Player(
-  "Папой1", "Диктатор2", 522, "немой2", "гольф2", "народ2", "микрофон2", "красивый2"
-)
-let player_4 = new Player(
-  "Папой1", "Диктатор3", 522, "немой3", "гольф2", "народ2", "микрофон2", "красивый2"
-)
-let player_5 = new Player(
-  "Папой1", "Диктатор4", 522, "немой4", "гольф2", "народ2", "микрофон2", "красивый2"
-)
-let player_6 = new Player(
-  "Папой1", "Диктатор5", 522, "немой5", "гольф2", "народ2", "микрофон2", "красивый2"
-)
-let player_7 = new Player(
-  "Папой1", "Диктатор6", 522, "немой6", "гольф2", "народ2", "микрофон2", "красивый2"
-)
-let player_8 = new Player(
-  "Папой1", "Диктатор7", 522, "немой7", "гольф2", "народ2", "микрофон2", "красивый2"
-)
+// let player_1 = new Player(
+//   "Папой", "Папо0оооой", 52, "немой", "гольф", "народ", "микрофон", "красивый"
+// )
+// let player_2 = new Player(
+//   "Бобр", "Землекоп", 15, "котузия", "есть коренья", "киянки", "запасные зубы", "пухлый"
+// )
+// let player_3 = new Player(
+//   "Папой1", "Диктатор2", 522, "немой2", "гольф2", "народ2", "микрофон2", "красивый2"
+// )
+// let player_4 = new Player(
+//   "Папой1", "Диктатор3", 522, "немой3", "гольф2", "народ2", "микрофон2", "красивый2"
+// )
+// let player_5 = new Player(
+//   "Папой1", "Диктатор4", 522, "немой4", "гольф2", "народ2", "микрофон2", "красивый2"
+// )
+// let player_6 = new Player(
+//   "Папой1", "Диктатор5", 522, "немой5", "гольф2", "народ2", "микрофон2", "красивый2"
+// )
+// let player_7 = new Player(
+//   "Папой1", "Диктатор6", 522, "немой6", "гольф2", "народ2", "микрофон2", "красивый2"
+// )
+// let player_8 = new Player(
+//   "Папой1", "Диктатор7", 522, "немой7", "гольф2", "народ2", "микрофон2", "красивый2"
+// )
 
 
-let playersMas = [
-  player_1, player_2, player_3, player_4, player_5, player_6, player_7, player_8
-]
+let playersMas = []
+
+for (let i = 0; i < 8; i++) {
+  let name = namesMas[random(0,namesMas.length)]
+  let prof = professionMas[random(0,professionMas.length)]
+  let disease = diseaseMas[random(0,diseaseMas.length)]
+  let workExp = random(0, 80)
+  let age = random(0, 100)
+  let hobby = hobbyMas[random(0,hobbyMas.length)]
+  let fobos = fobosMas[random(0,fobosMas.length)]
+  let ability = abilityMas[random(0,abilityMas.length)]
+  
+playersMas.push( new Player(
+  name, age, prof, workExp, disease, hobby, fobos,"ничего нет в карманцах", ability  
+) )
+
+}
 
 
 
-let widthLine = 100
-let widthMan = 120
-let widthMic = 90
+let widthLine = 85
+let widthMan = 105
+let widthMic = 80
 
 
 
-
+let count = 0
 
 for (let i = 0; i < playersMas.length; i++) {
   
@@ -97,10 +132,20 @@ for (let i = 0; i < playersMas.length; i++) {
           </div>
         `)
 
-
+if (count == 1) {
   widthLine = widthLine - 15
-  widthMan = widthMan - 10
-  widthMic = widthMic - 10
+  widthMan = widthMan - 15
+  widthMic = widthMic - 15
+  count = 0
+}
+else {
+
+  count++
+}
+
+
+
+
 
 }
 
@@ -114,7 +159,15 @@ players.forEach((player,i) => {
   player.onclick = ()=>{
     console.log(playersMas[i].profession);
     menu.innerHTML = `
-    <div class="room-menu__item">
+      <div class="room-menu__item">
+        <h2>Имя: ${playersMas[i].name}</h2>
+        <img src="./assets/images/icons/question-mark.svg" alt="" />
+      </div>
+      <div class="room-menu__item">
+        <h2>Возраст: ${playersMas[i].age.age}</h2>
+        <img src="./assets/images/icons/question-mark.svg" alt="" />
+      </div>
+      <div class="room-menu__item">
         <h2>Профессия: ${playersMas[i].profession.profession}</h2>
         <img src="./assets/images/icons/question-mark.svg" alt="" />
       </div>
