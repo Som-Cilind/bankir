0d228f7ac706af5d965499b3fb87300e6d8a1dc5
Som-Cillindr <danpapx@gmail.com>
2024-06-12 20:38:52 +1000

    мчвслльяывсв

--- бей-бобра/assets-bobr/bobr.js
+++
@@ -1,8 +0,0 @@
-let button_start = document.querySelector(".начать-б")
-
-button_start.addEventListener("click",=() =>{
-   function name(params) {
-    darkBlock.style.display  = "none"
-   }
-
-})
